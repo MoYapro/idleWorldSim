13cbcbaac0aeba0b32c9a4abf9ab3f05f4187a60
Tom Ritmeierr <tom.ritmeier@gmail.com>
2020-04-04 06:52:45 +0200

    generify applyTo extension function to work on any list not only for List<Trait>

--- app/src/main/java/de/moyapro/idle/util/ListExtensions.kt
+++ app/src/main/java/de/moyapro/idle/util/ListExtensions.kt
@@ -1,9 +1,19 @@
 package de.moyapro.idle.util
 
-import de.moyapro.idle.domain.traits.Trait
+import kotlin.reflect.KFunction2
 
-inline fun <reified T : Trait, V : Any> List<T>.applyTo(initialValue: V, influenceMethod: T.(V) -> V): V {
-    return this.filterIsInstance<T>()
-        .fold(initialValue)
-        { modifiedValue, listElement: Trait -> influenceMethod.invoke(listElement, modifiedValue) }
+inline fun <reified T : Any, V : Any, reified X : T> List<T>.applyTo(initialValue: V, influenceMethod: KFunction2<X, V, V>): V {
+    var x = initialValue
+    this.forEach {
+        if (it is X) // since covariance is not allowed in extensionfunctions we use if to check if T is subclass of X
+            x = influenceMethod.invoke(it, initialValue)
+    }
+    return x
 }
+
+// This is just for demonstration that kotlin compiler's type inference is still not perfect. Code should be semanticaly identical to working code above
+//inline fun <reified T : Any, V : Any, reified X : T> List<T>.applyTo(initialValue: V, influenceMethod: KFunction2<X, V, V>): V {
+//    return this.filterIsInstance<X>()
+//        .fold(initialValue)
+//        { modifiedValue, listElement: T -> influenceMethod.invoke(listElement, modifiedValue) }
+//}
