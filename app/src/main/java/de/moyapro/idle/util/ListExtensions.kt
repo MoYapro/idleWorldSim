36ffc7b68810314425f7d77a1aa6ca5ab262b542
Arne Klaaßen <arne.klaassen@gmx.de>
2020-04-04 20:26:29 +0200

    removed useless comment

--- app/src/main/java/de/moyapro/idle/util/ListExtensions.kt
+++ app/src/main/java/de/moyapro/idle/util/ListExtensions.kt
@@ -2,7 +2,6 @@ package de.moyapro.idle.util
 
 import kotlin.reflect.KFunction2
 
-// This is just for demonstration that kotlin compiler's type inference is still not perfect. Code should be semantically identical to working code above
 inline fun <reified T : Any, V : Any, reified X : T> Collection<T>.applyTo(initialValue: V, influenceMethod: KFunction2<X, V, V>): V {
     return this.filterIsInstance<X>()
         .fold(initialValue)
