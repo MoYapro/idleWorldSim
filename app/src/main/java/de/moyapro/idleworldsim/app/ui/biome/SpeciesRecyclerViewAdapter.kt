1bb2db9abaf1b819a3370c376dfab6c8536b4c88
Arne Klaaßen <arne.klaassen@gmx.de>
2020-04-18 23:46:42 +0200

    population in million

--- app/src/main/java/de/moyapro/idleworldsim/app/ui/biome/SpeciesRecyclerViewAdapter.kt
+++ app/src/main/java/de/moyapro/idleworldsim/app/ui/biome/SpeciesRecyclerViewAdapter.kt
@@ -55,7 +55,7 @@ class SpeciesRecyclerViewAdapter(
         val species = biome.getSpecies()[position]
         val population = biome.resources[species]
         holder.mIdView.text = species.name
-        holder.mContentView.text = population.toShortDecimalStr()
+        holder.mContentView.text = (population * 1E6).toShortDecimalStr()
 
         with(holder.mView) {
             tag = species
