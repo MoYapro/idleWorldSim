8a4ccb3b4c9ce72eb906a4e513dd4f13bd4356d6
Tom Ritmeierr <tom.ritmeier@gmail.com>
2020-05-02 06:08:36 +0200

    add population equals and hashCode

--- app/src/main/java/de/moyapro/idleworldsim/domain/valueObjects/Population.kt
+++ app/src/main/java/de/moyapro/idleworldsim/domain/valueObjects/Population.kt
@@ -1,6 +1,6 @@
 package de.moyapro.idleworldsim.domain.valueObjects
 
-inline class Population(val populationSize: Double) {
+class Population(val populationSize: Double) {
     operator fun compareTo(other: Population) = populationSize.compareTo(other.populationSize)
     operator fun plus(other: Population) = Population(this.populationSize + other.populationSize)
     operator fun times(growthRate: GrowthRate) = Population(populationSize * growthRate.rate)
@@ -8,6 +8,15 @@ inline class Population(val populationSize: Double) {
     operator fun minus(other: Population) = Population(populationSize - other.populationSize)
     operator fun div(divider: Double) = Population(populationSize / divider)
     operator fun times(scalar: Double) = Population(populationSize * scalar)
+
+    override fun equals(other: Any?): Boolean {
+        return other is Population && populationSize.equals(other.populationSize)
+    }
+
+    override fun hashCode(): Int {
+        return populationSize.hashCode()
+    }
+
 }
 
 fun Iterable<Population>.sum(): Population {
