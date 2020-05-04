3f392e09f385c66174b0704f86f554aef06c56b1
Tom Ritmeierr <tom.ritmeier@gmail.com>
2020-05-04 07:05:36 +0200

    add test to ensure that only available resources were used
    (cherry picked from commit 148f329b9b0ef65c8cc485fd0856acc7c2a32b84)

--- app/src/test/java/de/moyapro/idleworldsim/domain/RessourceDistributionTest.kt
+++ app/src/test/java/de/moyapro/idleworldsim/domain/RessourceDistributionTest.kt
@@ -7,6 +7,7 @@ import de.moyapro.idleworldsim.domain.valueObjects.Population
 import de.moyapro.idleworldsim.domain.valueObjects.Resource
 import de.moyapro.idleworldsim.domain.valueObjects.ResourceType.Minerals
 import de.moyapro.idleworldsim.domain.valueObjects.ResourceType.Water
+import de.moyapro.idleworldsim.util.sumUsing
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -34,13 +35,15 @@ class RessourceDistributionTest {
     fun calculateAvailableResourcesEqualShare() {
         val species1 = Species("one").evolve(ConsumerTrait(Water), NeedResource(Water))
         val species2 = Species("two").evolve(ConsumerTrait(Water), NeedResource(Water))
-        val available = Resource(Water, 10.0)
-        val biome = Biome(resources = Resources(listOf(available)))
+        val available = Resources(listOf(Resource(Water, 10.0)))
+        val biome = Biome(resources = available)
             .settle(species1, Population(10.0))
             .settle(species2, Population(15.0))
-        val availableResources = biome.getAvailableResourcesPerSpecies()
-        assertThat(availableResources[species1]?.get(Water)?.amount ?: 0.0).isEqualTo(5.0)
-        assertThat(availableResources[species2]?.get(Water)?.amount ?: 0.0).isEqualTo(5.0)
+        val distributedResources = biome.getAvailableResourcesPerSpecies()
+        assertThat(distributedResources.values.sumUsing(Resources::plus)?.getQuantities()?.all { used -> used.amount < available[used.resourceType].amount })
+            .`as`("Should not ave distributed more that is availble")
+        assertThat(distributedResources[species1]?.get(Water)?.amount ?: 0.0).isEqualTo(5.0)
+        assertThat(distributedResources[species2]?.get(Water)?.amount ?: 0.0).isEqualTo(5.0)
     }
 
 }
