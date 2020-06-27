f8715e7103b4e4f15eab8f4b92b979c8cc8f9099
Tom Ritmeier <tom.ritmeier@gmail.com>
2020-06-27 07:32:06 +0200

    create test for comsume factor and preferences

--- app/src/test/java/de/moyapro/idleworldsim/domain/consumption/CalculateConsumeFactorTest.kt
+++ app/src/test/java/de/moyapro/idleworldsim/domain/consumption/CalculateConsumeFactorTest.kt
@@ -1,15 +1,29 @@
 package de.moyapro.idleworldsim.domain.consumption
 
+import de.moyapro.idleworldsim.domain.traits.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CalculateConsumeFactorTest {
 
     @Test
-    fun foobar() {
-        val consumer: ResourceConsumer
-        val producerWithoutConter: ResourceProducer
-        val producerWithCounter: ResourceProducer
+    fun countersReducePowerFactor() {
+        val producerWithoutConter: ResourceProducer = SpeciesImpl("producerWithoutCounter", Feature(Meaty))
+        val producerWithCounter: ResourceProducer = SpeciesImpl("producerWithCounter", Feature(Meaty, Stealth()))
+        val consumer: ResourceConsumer = SpeciesImpl("consumer", Feature(Predator(Meaty), Vision()))
+        val powerFactorWithCounter = consumer.consumePowerFactor(producerWithCounter)
+        val powerFactorWithoutCounter = consumer.consumePowerFactor(producerWithoutConter)
+        assertThat(powerFactorWithCounter).isGreaterThan(powerFactorWithoutCounter)
+    }
 
+    @Test
+    fun countersReduceConsumerPreference() {
+        val producerWithoutConter: ResourceProducer = SpeciesImpl("producerWithoutCounter", Feature(Meaty))
+        val producerWithCounter: ResourceProducer = SpeciesImpl("producerWithCounter", Feature(Meaty, Stealth()))
+        val consumer: ResourceConsumer = SpeciesImpl("consumer", Feature(Predator(Meaty), Vision()))
+        val factorWithCounter = consumer.calculatePreferenceIndex(producerWithCounter)
+        val factorWithoutCounter = consumer.calculatePreferenceIndex(producerWithoutConter)
+        assertThat(factorWithCounter).isGreaterThan(factorWithoutCounter)
     }
 
 }
