46f0975aeb70d8bdc447ba2248ba1ea7a0a1ed3e
Tom Ritmeier <tom.ritmeier@gmail.com>
2020-06-28 14:33:30 +0200

    implement species bases on predator traits

--- app/src/main/java/de/moyapro/idleworldsim/domain/consumption/SpeciesImpl.kt
+++ app/src/main/java/de/moyapro/idleworldsim/domain/consumption/SpeciesImpl.kt
@@ -1,6 +1,7 @@
 package de.moyapro.idleworldsim.domain.consumption
 
 import de.moyapro.idleworldsim.domain.traits.Feature
+import de.moyapro.idleworldsim.domain.traits.Predator
 
 open class SpeciesImpl(
     override val name: String,
@@ -9,11 +10,14 @@ open class SpeciesImpl(
     constructor(name: String, vararg features: Feature) : this(name, listOf(*features))
 
     override val creator = { name: String, features: List<Feature> -> SpeciesImpl(name, features) }
+
     @Deprecated("This should not be used any more. CanConsume() should be based on traits")
     private val canConsume: MutableList<String> = mutableListOf()
-    override fun canConsume(producer: ResourceProducer): Boolean {
-        return canConsume.contains(producer.name)
-    }
+    override fun canConsume(producer: ResourceProducer) =
+        traits()
+            .filterIsInstance<Predator>()
+            .map { it.preyTrait }
+            .any { producer.traits().contains(it) }
 
     override fun equals(other: Any?): Boolean {
         return if (null == other || other !is SpeciesImpl) {
